14a623e5a4eb21b07ce088da6362506d88d709ed
Huntbase <152288822+Huntbase@users.noreply.github.com>
2024-06-21 02:47:08 +0700

    Data

---
+++ Data Perusahaan/users.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 20, 2024 at 09:46 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pettopia`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `photo` varchar(255) NOT NULL DEFAULT 'default.svg',
+  `role` varchar(10) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `name`, `photo`, `role`) VALUES
+(1, '123', '123@gmail.com', '$2y$10$E.H2Lwv5dC1tITGGoNzHMuc2wQf5cZ7jxBKNXtPpC2klqI/k7yKkq', '123', 'default.svg', 'user'),
+(2, '1321', '12321@gmail.com', '$2y$10$ikwkZRjrFMuiInUMVNuTGOxfK/F.yXBy0ieZ16U9bzCmxFIriXHSG', '123213', 'default.svg', 'user'),
+(6, '1235', '213@gmail.com', '$2y$10$3s6ehtw2gMK3U5nYEHT3xuR/PIXiqkpGs4FGUoEuzK56iHSz3IC6G', '214321', 'default.svg', 'user'),
+(11, 'admin', 'admin@gmail.com', '$2y$10$iB0w2tO8tUCjujYy2kdj0ecNavQabnP5XvWHpaX9m0Ge1dUygWuOi', 'admin', 'default.svg', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
