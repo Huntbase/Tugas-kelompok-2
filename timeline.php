221baf3c65b3fd6a3c8f422209e7c5f48381e3ba
Huntbase <152288822+Huntbase@users.noreply.github.com>
2024-06-21 02:17:36 +0700

    Menuju Profil

--- timeline.php
+++ timeline.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pesbuk Timeline</title>
+    <title>Akun</title>
 
     <link rel="stylesheet" href="css/bootstrap.min.css" />
 </head>
